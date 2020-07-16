4be2510727508fc0d6b1aa8c2e9f1306e118f89f
huihui <huihui@example.com>
2020-07-16 18:16:46 +0800

    make classNumber a class

--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -1,15 +1,15 @@
 package com.thoughtworks.basic;
 
 public class Student extends Person{
-    private int classNumber;
+    private ClassNumber classNumber;
 
-    public Student(String name, int age, int classNumber) {
+    public Student(String name, int age, ClassNumber classNumber) {
         super(name,age);
         this.classNumber = classNumber;
     }
 
     public String introduce(){
-        String studentIntroduce = "My name is " + name +". I am " + age + " years old. " + "I am a Student of Class " + classNumber + ".";
+        String studentIntroduce = "My name is " + name +". I am " + age + " years old. " + "I am a Student of Class " + classNumber.getNumber() + ".";
         return studentIntroduce;
     }
 }
--- src/test/java/com/thoughtworks/basic/StudentTest.java
+++ src/test/java/com/thoughtworks/basic/StudentTest.java
@@ -7,12 +7,13 @@ public class StudentTest {
     @Test
     public void should_return_studentIntroduce_given_student_when_introduce() {
         //given
-        Student student = new Student("lily",24,1);
+        ClassNumber classNumber = new ClassNumber(2);
+        Student student = new Student("lily",24,classNumber);
 
         //when
         String studentIntroduce = student.introduce();
 
         //then
-        Assert.assertEquals("My name is lily. I am 24 years old. I am a Student of Class 1.",studentIntroduce);
+        Assert.assertEquals("My name is lily. I am 24 years old. I am a Student of Class 2.",studentIntroduce);
     }
 }
